b584c41d017d4e699ce2ea305fe2c6bd80833a91
Mary_Ouma <maryawish54@gmail.com>
2022-09-08 06:31:54 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
- * main
+ * main prints out stdout
  *
  * returns 0 when successful
- * 
  */
 
 int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,");
 
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
