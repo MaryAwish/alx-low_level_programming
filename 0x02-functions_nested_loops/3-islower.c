cceb72f2d2ffb8f2fac9b80932fb24740dc10098
Mary_Ouma <maryawish54@gmail.com>
2022-09-12 22:09:05 -0700

    Is lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * Descrription:  _islower - checks for lowercase character
+ *
+ * Return: Always 0
+*/
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 Functions and nested loops
 Task 2: letters
 Task 2: Letters x 10
+ Task 3: function that checks for lowercase
