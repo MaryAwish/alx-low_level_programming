60d492fc08c1340f33d65df95d7c3809a89f5c37
Mary_Ouma <maryawish54@gmail.com>
2022-09-12 23:09:15 -0700

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * Description: _isalpha - prints uppercase
+ *
+ * @c: letter tested
+ * Return: Always 0
+*/
+
+int _isalpha(int c)
+{
+	if ((c = 97 && c <= 122) || (c = 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -2,3 +2,4 @@ Functions and nested loops
 Task 2: letters
 Task 2: Letters x 10
  Task 3: function that checks for lowercase
+Task 4: is alpha
