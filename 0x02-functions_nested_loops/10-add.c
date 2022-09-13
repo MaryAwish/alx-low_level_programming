13e6eba6649644a957acc9c9ffc45aa6b224264c
Mary_Ouma <maryawish54@gmail.com>
2022-09-13 12:36:42 -0700

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds integers
+ * @i: integer
+ * @j: integer
+ * Return: returns sum
+*/
+
+int add(int i, int j)
+{
+	return (i + j);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks code
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}
