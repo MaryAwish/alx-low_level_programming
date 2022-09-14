da5fdb70db7d52ac37d15fbe45ecb1ac173d9646
Mary_Ouma <maryawish54@gmail.com>
2022-09-14 06:25:50 -0700

    Fibonacci 1

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints the fibonacci sequence of 1st 50 numbers
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int k;
+	long int t1 = 0, t2 = 1, sum;
+
+	for (k = 0; k <= 50; k++)
+	{
+		sum = t1 + t2;
+		printf("%ld", sum);
+
+		ti = t2;
+		t2 = sum;
+
+		if (k == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return(0);
+}
