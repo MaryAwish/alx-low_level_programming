a41d291b507c0acac9de12a901547b15a2a63ad7
Mary_Ouma <maryawish54@gmail.com>
2022-09-14 11:37:31 -0700

    something

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,19 +8,31 @@
 
 int main(void)
 {
-	long int i, t1 = 1, t2 = 2, sum = 0;
+	long int i, f = 1, a = 2, l = 1000000000, f1, f2, a1, a2;
 
-	for (i = 1; i < 98; i++)
+	printf("%ld", f);
+
+	for (i = 1; i < 91; i++)
 	{
-		printf("%ld, ", t1);
-		sum = t1 + t2;
-		t1 = t2;
-		t2 = sum;
-
-		if (i == 97)
-		{
-			printf("%ld\n", t1);
-		}
+		printf(", %ld", a);
+		a += f;
+		f = a - f;
 	}
+
+	f1 = (f / l);
+	f2 = (f % l);
+	a1 = (a / l);
+	a2 = (a % l);
+
+	for (i = 92; i < 99; i++)
+	{
+		printf(", %ld", a1 + (a2 / l));
+		printf("%ld", a2 % l);
+		a1 = a1 + f1;
+		f1 = a1 - f1;
+		a2 = a2 + f2;
+		f2 = a2 - f2;
+	}
+	printf("\n");
 	return (0);
 }
