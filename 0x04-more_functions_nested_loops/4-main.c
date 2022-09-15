a58cb103ea80d86a2ca8b64ed9458482380e7f3e
Mary_Ouma <maryawish54@gmail.com>
2022-09-15 09:04:33 -0700

    Numbers

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - checks code
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	print_most_numbers();
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints most numbers
+ *
+ * Return: Always 0
+*/
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		if (c != 2 && c != 4)
+		{
+			_putchar(c + '0');
+		}
+	}
+	_putchar('\n');
+}
