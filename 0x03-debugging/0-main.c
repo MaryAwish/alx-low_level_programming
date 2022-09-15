546e2a3a66c31f0aea19d90d4f00a69328e298ed
Mary_Ouma <maryawish54@gmail.com>
2022-09-15 02:26:51 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is +ve 0r -ve
+ *
+ * Return: always 0
+*/
+
+int main(void);
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
