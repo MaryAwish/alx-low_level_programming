7461a853515d9157635743614e3c2c550a4f108f
Mary_Ouma <maryawish54@gmail.com>
2022-09-15 03:20:36 -0700

    Done

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -5,25 +5,28 @@
 /**
  * positive_or_negative - checks code
  *
+ * @i: input
  * Return: always 0
 */
 
-void positive_or_negative(int n)
+void positive_or_negative(int i)
 
 {
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	int i;
 
-	if (n > 0)
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+
+	if (i > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive\n", i);
 	}
-	else if (n == 0)
+	else if (i == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", i);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%d is negative\n", i);
 	}
 }
