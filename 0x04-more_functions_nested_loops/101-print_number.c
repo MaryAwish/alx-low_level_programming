10b156f2cdd813a8a99c4eed688ed4451539d253
Mary_Ouma <maryawish54@gmail.com>
2022-09-15 15:21:56 -0700

    Final

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int x, y, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		x = -n;
+	}
+	else
+	{
+		x = n;
+	}
+	y = x;
+	count = 1;
+
+	while (y > 9)
+	{
+		y /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((x / count) % 10) + 48);
+	}
+}
