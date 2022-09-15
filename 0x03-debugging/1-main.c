deb1d6efa99404922d5f2e2df3ee9045a6e749c7
Mary_Ouma <maryawish54@gmail.com>
2022-09-15 04:46:20 -0700

    Task 2

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	* while (i < 10)
+	* {
+	*	 putchar(i);
+	* }
+	*/
+
+	printf("Infinite loop avoided! \\0/\n");
+
+	return (0);
+}
