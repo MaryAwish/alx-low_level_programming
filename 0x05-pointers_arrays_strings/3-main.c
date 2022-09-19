1f436175a4969249e3e13be810e10a6d76daaf6e
Mary_Ouma <maryawish54@gmail.com>
2022-09-19 03:43:39 -0700

    A string

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - checks the code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
