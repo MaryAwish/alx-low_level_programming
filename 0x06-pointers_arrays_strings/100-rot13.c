6891e6e3e385ed277593d7058995add90d03b740
Mary_Ouma <maryawish54@gmail.com>
2022-09-22 05:40:18 -0700

    Rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,10 +11,10 @@ char *rot13(char *s)
 {
 	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
-		for (i = 0; (s[i] >= 'a' && s[i] <= 'z')
-				|| (s[i] >= 'A' && s[i] <= 'Z';)i++)
+		while ((s[i] >= 'a' && s[i] <= 'z')
+				|| (s[i] >= 'A' && s[i] <= 'Z'))
 		{
 			if ((s[i] >= 'a' && s[i] <= 'm') ||
 					(s[i] >= 'A' && s[i] <= 'M'))
@@ -25,7 +25,9 @@ char *rot13(char *s)
 			{
 				s[i] -= 13;
 			}
+			i++;
 		}
+		i++;
 	}
 	return (s);
 }
