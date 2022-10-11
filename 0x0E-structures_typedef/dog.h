4b2f6207639a50455bdf237d5486535975028440
Mary_Ouma <maryawish54@gmail.com>
2022-10-11 00:44:52 -0700

    Dog attributes

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog attributes
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: The attributes of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
