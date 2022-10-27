fbef2effb1223922d95276d657fa577fad2d421f
Mary_Ouma <maryawish54@gmail.com>
2022-10-27 05:32:45 -0700

    Task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,30 @@
 #include "lists.h"
 
 /**
+ * freelist - frees linkedlist
+ * @head: heead of a list
+ *
+ * Return: void
+ */
+
+void freelist(listp_t **head)
+{
+	listp_t *tmnp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((tmnp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(tmnp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
  * print_listint_safe - prints a linked list, safely
  * @head: list of type listint_t to print
  *
@@ -9,21 +33,36 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t num = 0;
-	long int diff;
+	listp_t *ptr, *new, *add;
 
-	while (head)
+	ptr = NULL;
+	while (head != NULL)
 	{
-		diff = head - head->next;
-		num++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (diff > 0)
-			head = head->next;
-		else
-		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
-		}
-	}
+		new = malloc(sizeof(listp_t));
 
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)head;
+		new->next = ptr;
+		ptr = new;
+
+		add = ptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				freelist(&ptr);
+				return (num);
+			}
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		num++;
+	}
+	freelist(&ptr);
 	return (num);
 }
