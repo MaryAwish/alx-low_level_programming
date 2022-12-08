4e36af4bf7fe8faa58d28f12701d3794f0ace622
Mary_Ouma <maryawish54@gmail.com>
2022-12-08 03:28:02 -0800

    2 elements

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints elemnts of dlistint_t list
+ *
+ * @h: pointer to the start of the linked list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
