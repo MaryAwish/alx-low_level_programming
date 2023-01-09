0236527ed308963539c3bc490b344d561b058b51
Mary Ouma <maryawish54@gmail.com>
2023-01-08 18:04:28 -0800

    hash1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table->array = malloc(size * sizeof(hash_node_t *));
 	if (hash_table->array == NULL)
 	{
-		free(hash_table);
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
